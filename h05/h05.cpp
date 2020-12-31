7b57293d4e3e1627e90470605ef5ed7973a418ad
Stephen Gilbert <sgilbert@student.cccd.edu>
2020-12-31 23:48:24 +0000

    auto commit 12/31/20 23:48:24

---
+++ h05/h05.cpp
@@ -0,0 +1,23 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h05.cpp
+ */
+#include <string>
+using namespace std;
+
+string STUDENT = "WHO AM I?";  // Add your Canvas login name
+
+// Write your function here
+string toFrenchGender(const string& country)
+{
+    return country;
+}
+
+/////////////// Optional Student Code /////////////////
+int run()
+{
+    // You may add any code you like
+    // It will be displayed with make run
+    return 0;
+}
